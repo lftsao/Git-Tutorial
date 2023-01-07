f940ca5571a5f93c4d8335dc9ab3815e3b10b182
Ling Fan Tsao <lf.tsao@gmail.com>
2023-01-07 19:36:44 +0800

    Initial Commit

---
+++ Git Tutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Git Tutorial
+//
+//  Created by Kevin Tsao on 2023/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Git!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Tutorial/Git_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TutorialApp.swift
+//  Git Tutorial
+//
+//  Created by Kevin Tsao on 2023/1/7.
+//
+
+import SwiftUI
+
+@main
+struct Git_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
