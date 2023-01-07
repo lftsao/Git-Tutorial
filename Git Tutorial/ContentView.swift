0b9effb7f14f754c94583332df0439cb8382e823
Ling Fan Tsao <lf.tsao@gmail.com>
2023-01-07 20:24:29 +0800

    Add wording named Git Push!

--- Git Tutorial/ContentView.swift
+++ Git Tutorial/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, Git!")
             Text("Test branch!")
+            Text("Test Push!!!")
         }
         .padding()
     }
